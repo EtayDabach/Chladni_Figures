dba440f4f055c5520405c1697b8ea45ab488ebff
Etay <etay.dabach@gmail.com>
2024-12-08 17:43:04 +0200

    Add time dependet equation to explanation, new library for future gui option (testing in new ignored file).

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 import scipy.special as sps
 from matplotlib import animation
 import ipywidgets as widg
+from IPython.display import HTML
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
